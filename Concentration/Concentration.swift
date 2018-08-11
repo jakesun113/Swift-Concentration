9a9e1d4c7f24a45fff0ef97b04fff1bd927018b3
wry <wry@wrydemacbook-pro.local>
2018-08-11 17:18:07 +1000

    add closure function

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -21,22 +21,25 @@ class Concentration
     {
         get
         {
-            var foundIndex: Int?
-            for index in cards.indices
-            {
-                if cards[index].isFaceUp
-                {
-                    if foundIndex == nil
-                    {
-                        foundIndex = index
-                    }
-                    else
-                    {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            //add closure function
+            let faceUpCardIndices = cards.indices.filter{cards[$0].isFaceUp}
+            return faceUpCardIndices.count == 1 ? faceUpCardIndices.first: nil
+//            var foundIndex: Int?
+//            for index in cards.indices
+//            {
+//                if cards[index].isFaceUp
+//                {
+//                    if foundIndex == nil
+//                    {
+//                        foundIndex = index
+//                    }
+//                    else
+//                    {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         
         set(newValue)
